10cf190ede967a2774dfc89e87d2e17be08b2b17
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-10 14:08:08 -0500

    Set up, Day 2

---
+++ src/day02_print_statements/Comments.java
@@ -0,0 +1,29 @@
+package day02_print_statements;
+
+public class Comments {
+
+    // this is single line comment
+    // System.out.println("Hello World"); --> this does not run, this is comment, just text
+
+    /*
+        This is multi line comment
+        I can add as many lines as I want
+        Hello
+        Variables Monday
+     */
+
+    /**
+     *  this is for java doc comments
+     *
+     * helps explain different part of java
+     *
+     * we will better be able to understand why we need this one
+     * *
+     */
+
+    // TODO: put main method
+
+    // TODO: practice printing
+
+
+}
---
+++ src/day02_print_statements/TellMeAboutYourself.java
@@ -0,0 +1,19 @@
+package day02_print_statements;
+
+public class TellMeAboutYourself {
+    /*
+    Create a new class: TellMeAboutYourself in today's package
+	create main method
+	print your name
+	print your city
+	print your favorite food
+     */
+
+    public static void main(String [] args){
+        System.out.println("Saim");
+        System.out.println("Chicago");
+        System.out.println("Corn");
+
+    }
+
+}
