5f12c07c66cb800afc256a56fba870711973ce62
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-13 14:41:34 -0500

    Day 3 Variables

---
+++ src/day03_variables/DateTomorrow.java
@@ -0,0 +1,22 @@
+package day03_variables;
+
+public class DateTomorrow {
+    public static void main(String[] args) {
+
+        /*
+            declare and assign 3 variables(numbers)
+            month, day, year
+
+            print the date in this format:
+                Date: month \ day \ year
+
+            also update the day for tomorrow date
+            print tomorrow's date in this format:
+               Tomorrow's Date: month \ day \ year
+
+
+         */
+
+
+    }
+}
