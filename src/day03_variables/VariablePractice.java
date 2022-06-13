8b245e786a2cf9c3857b87cfa0163ff348b4feae
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-13 12:02:55 -0500

    Day 3 Variables

---
+++ src/day03_variables/VariablePractice.java
@@ -0,0 +1,22 @@
+package day03_variables;
+
+public class VariablePractice {
+    public static void main(String[] args) {
+
+        byte age; // declared a byte variable that is called age
+        age = 10; // assigning 10 to the age variable
+        System.out.println(age); // printing the age variable, which means I will print the value of the variable
+        System.out.println("age"); // this prints the characters age, not the variable that has a number value
+        System.out.println("20"); // printing the character 2 and 0
+        System.out.println(20); // printing the int 20
+        // by default 20 is int type
+        System.out.println(age + 1); // age value is 10, then it adds 10 + 1 and prints 11
+
+        //byte num = 200; 200 is too big for byte types
+        short num = 200; // declared and assigned at the same time
+
+
+
+
+    }
+}
