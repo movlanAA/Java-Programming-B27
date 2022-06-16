d567b3da494f027c8cd209b9dd33cc84909cbca8
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-16 11:45:40 -0500

    Day 5

--- src/day05_arithmetic_operators/AgeCalc.java
+++ src/day05_arithmetic_operators/AgeCalc.java
@@ -4,7 +4,11 @@ public class AgeCalc {
     /*
         define your birth year variable
         calculate the age
-
-
      */
+    public static void main(String[] args) {
+
+        int birthYear = 1987;
+        int age = 2022 - birthYear;
+        System.out.println(age);
+    }
 }
