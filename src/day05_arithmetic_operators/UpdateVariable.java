4f8a900c8ae0ca38b1467b5e0730f8d350aee7c8
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-16 12:00:03 -0500

    Day 5

---
+++ src/day05_arithmetic_operators/UpdateVariable.java
@@ -0,0 +1,18 @@
+package day05_arithmetic_operators;
+
+public class UpdateVariable {
+    public static void main(String[] args) {
+
+        int a = 4;
+        System.out.println(a + 10);
+        System.out.println(a);
+
+        a = a + 20;
+        System.out.println(a);
+
+        int b = 5;
+        b += 30; // this is the same as this: b = b + 30;
+        System.out.println(b);
+
+    }
+}
