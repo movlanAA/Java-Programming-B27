3093e97c33bd39832ab12ba8e444cfe3564dc60a
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-21 10:50:38 -0500

    Day 7 recap

---
+++ src/day07_scanner/ShortCircuitEx.java
@@ -0,0 +1,22 @@
+package day07_scanner;
+
+public class ShortCircuitEx {
+    public static void main(String[] args) {
+
+        System.out.println(true && true);
+        System.out.println(false && true);
+        //System.out.println(5/0); you cannot divide by 0
+
+       // System.out.println(true && 5/0 == 0);
+        System.out.println(false && 5/0 == 0);
+
+        //System.out.println(false & 5/0 == 0);
+
+        System.out.println(true || false);
+
+        int count = 0;
+        System.out.println(true || count++ == 1);
+        System.out.println(count);
+
+    }
+}
