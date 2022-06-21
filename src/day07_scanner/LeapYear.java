711eb1c8bfc896c3131c3667d6d728eab9fee016
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-21 09:57:36 -0500

    Day 7 recap

---
+++ src/day07_scanner/LeapYear.java
@@ -0,0 +1,18 @@
+package day07_scanner;
+
+public class LeapYear {
+
+    /*
+    create a class LeapYear
+
+    declare and assign a year variable
+    check if the year is a leap year, a year will be a leap year if it is divisible by 4
+     */
+    public static void main(String[] args) {
+
+        int year = 2024;
+        boolean isLeapYear = year % 4  == 0;
+        System.out.println(isLeapYear);
+
+    }
+}
---
+++ src/day07_scanner/UpdateVariable.java
@@ -0,0 +1,17 @@
+package day07_scanner;
+
+public class UpdateVariable {
+    public static void main(String[] args) {
+      //  What is the value of a and b
+
+        int a = 8; // a = 8  -> a = 7
+        int b = a--; // b = 8
+
+//        what is a?
+//        what is b?
+
+//        int a = 8; // a = 8  -> a = 7
+//        int b = --a; // b = 7
+
+    }
+}
