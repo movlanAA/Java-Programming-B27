b524d9d9468e84a2b1978442fcea15155462cc30
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-24 10:49:00 -0500

    day 9 scanner

--- src/day09_if_statements/Bank.java
+++ src/day09_if_statements/Bank.java
@@ -3,6 +3,20 @@ package day09_if_statements;
 public class Bank {
     public static void main(String[] args) {
 
+        double balance = 1_000_000;
+        double amountToWithdraw = 1_500_000;
+
+        balance -= amountToWithdraw; // balance = balance - amountToWithdraw;
+        System.out.println("Withdrew: " + amountToWithdraw);
+
+        if(balance >= 0){
+            System.out.println("Account balance is: " + balance);
+        }
+
+        if(balance < 0){
+            System.out.println("Overdraft penalty");
+            System.out.println("penalty due: " + amountToWithdraw * 0.25);
+        }
 
 
     }
---
+++ src/day09_if_statements/Letters.java
@@ -0,0 +1,30 @@
+package day09_if_statements;
+
+public class Letters {
+
+    public static void main(String[] args) {
+
+        char letter = '#';
+
+        if(letter >= 'A' && letter <= 'Z'){
+            System.out.println(letter + " is an uppercase letter");
+        }
+
+        /*
+        if(letter >= 65 && letter <= 90){
+            System.out.println(letter + " is an uppercase letter");
+        }
+         */
+
+        if(letter >= 'a' && letter <= 'z'){
+            System.out.println(letter + " is an lowercase letter");
+        }
+
+        /*
+        if(letter >= 97 && letter <= 122){
+            System.out.println(letter + " is an lowercase letter");
+        }
+         */
+
+    }
+}
