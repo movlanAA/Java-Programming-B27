041fab66cc1bb2f4c3e3f5112f71d123fe46979b
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-24 13:39:34 -0500

    day 9 scanner

---
+++ src/day09_if_statements/LunchTime.java
@@ -0,0 +1,18 @@
+package day09_if_statements;
+
+public class LunchTime {
+    public static void main(String[] args) {
+
+        boolean isHungry = false;
+        boolean isLunchTime = false;
+
+        if (isLunchTime && isHungry) {
+            System.out.println("go eat lunch");
+            System.out.println("take a break");
+        } else {
+            System.out.println("Practice more java");
+        }
+
+
+    }
+}
