b9d09474e527b5bf5f40c67954369719bc409ddb
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-28 11:04:15 -0500

    day 11 nested if

---
+++ src/day11_nested/NestedExample.java
@@ -0,0 +1,42 @@
+package day11_nested;
+
+public class NestedExample {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        if(a){
+            System.out.println(1);
+
+            if(b){
+                System.out.println(2);
+            }
+
+        }
+
+        /*
+        when
+            a = true
+            b = true
+            output:
+                1
+                2
+        when
+            a = false
+            b = true
+            output:
+                no output
+
+        when
+            a = true
+            b = false
+            output:
+                1
+
+         */
+
+
+
+    }
+}
