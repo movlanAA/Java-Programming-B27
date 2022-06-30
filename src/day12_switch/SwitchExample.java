ec0a237eb818093e285f400ad4dd9714ca99c24e
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-30 10:32:44 -0500

    day 12 switch

---
+++ src/day12_switch/SwitchExample.java
@@ -0,0 +1,36 @@
+package day12_switch;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+
+        int day = 9; // number from 1 to 7, where 1 is monday and 7 is sunday
+
+        switch (day){
+            default: // acts like else, if no other case matches the default case will run
+                System.out.println("Not a valid day number, should 1 to 7");
+                break;
+            case 1: // if(day == 1)
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
+    }
+}
