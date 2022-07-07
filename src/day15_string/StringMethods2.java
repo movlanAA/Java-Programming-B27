fec7fc477c438d766cab857f0503ac0daac26809
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-07-07 10:48:16 -0500

    day 15 String

--- src/day15_string/StringMethods2.java
+++ src/day15_string/StringMethods2.java
@@ -23,5 +23,15 @@ public class StringMethods2 {
         String s = "hello world today";
         System.out.println("word.startsWith(hello wor) " + s.startsWith("hello wor"));
 
+        System.out.println("------------------------");
+
+        String str = "today it is 80 degrees";
+        System.out.println(str);
+        System.out.println("str.contains(today)) "+ str.contains("today"));
+        System.out.println("str.contains(it is)) "+ str.contains("it is"));
+        System.out.println("str.contains(degrees)) "+ str.contains("degrees"));
+        System.out.println("str.contains(it 80)) exact sequence "+ str.contains("it 80")); // contains(it) && contains(80)
+        System.out.println(str.contains("80") && str.contains("today"));
+
     }
 }
