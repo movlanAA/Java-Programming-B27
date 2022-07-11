f099b8c44041ea9c3844f1984c10b9c9fd8200e8
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-07-11 11:54:43 -0500

    day 17 String

---
+++ src/day17_string/PrintLetters.java
@@ -0,0 +1,41 @@
+package day17_string;
+
+public class PrintLetters {
+    public static void main(String[] args) {
+
+        char first = 'A';
+
+        while (first <= 'Z') {
+            System.out.print(first++);
+        }
+
+        System.out.println();
+
+        // prinintg z - a
+        char backwards = 'z';
+        while(backwards >= 'a'){
+            System.out.print(backwards--);
+        }
+
+        /*
+
+            iteration 1:
+                first = 'A'
+                   'A' < 'Z' true
+                   loop runs
+                   prints(first) -> A
+                   then increments because of post increment
+                   first++ -> B
+
+            iteration 2:
+                first = B
+                    B < Z true
+                    loop runs
+                    print(first) -> B
+                    first++ -> C
+
+         */
+
+
+    }
+}
