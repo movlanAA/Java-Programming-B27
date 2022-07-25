0fc44b251d7e5871d8025727b5f6f89fe0f543ce
saim_cydeo <saim@cybertekschool.com>
2022-07-25 11:43:39 -0400

    day 25 arrays

--- src/day25_array/ArraysCopy.java
+++ src/day25_array/ArraysCopy.java
@@ -20,9 +20,22 @@ public class ArraysCopy {
         System.out.println(Arrays.toString(nums));
         System.out.println(Arrays.toString(other));
 
-        int [] arr = {3, 4, 1, 5,12, 5, -10};
+        int[] arr = {3, 4, 1, 5, 12, 5, -10};
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
 
+        // copyOf
+
+        int[] a = {5, 10, 15};
+        int[] b = Arrays.copyOf(a, 3); // new array with size 3
+        System.out.println("BEFORE");
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+        a[1] = 100;
+        System.out.println("AFTER");
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+
+        // we have two arrays, but we only change the middle element of the first array, but because they were seperate arrays the second one is untouched
     }
 }
