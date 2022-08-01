fed13961128a3b55f5430a7f44babe040e3c1317
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-08-01 13:36:18 -0500

    day 29 methods

--- src/my_utils/StringUtil.java
+++ src/my_utils/StringUtil.java
@@ -53,4 +53,23 @@ public class StringUtil {
 
     }
 
+        /*
+        given a String and char determine how many times the given char is found in the String
+        "java"
+        a
+
+        > 2
+     */
+
+    public static int frequencyOfChar(String str, char letter){
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == letter){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
 }
