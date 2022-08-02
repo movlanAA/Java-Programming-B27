1c5c5bad78bdab56c68bd3c013e9092d95fe56c1
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-08-02 15:11:45 -0500

    day 30 wrapper class

--- src/day30_practice_wrapper/Etsy.java
+++ src/day30_practice_wrapper/Etsy.java
@@ -5,6 +5,12 @@ public class Etsy {
 
         String s = "Item(s) total: $144.99";
         double price = Double.parseDouble(s.split(" ")[2].substring(1));
+        /*
+                s.split(" ") -> gives array ["Item(s)", "total:", "$144.99"]
+                s.split(" ")[2] --> $144.99
+                .substring(1) --> remove the dollar sign : 144.99
+         */
+
         System.out.println(price);
 
         if(price >= 100){
