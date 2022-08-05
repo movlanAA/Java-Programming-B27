1fef857e549a9db58953e6f375ca5cd1b978c8c1
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-08-05 09:43:50 -0500

    day 31 ArrayList

--- src/day32_arraylist/PracticeFlow.java
+++ src/day32_arraylist/PracticeFlow.java
@@ -70,8 +70,18 @@ Print the ArrayList after each action to see how the change is made
         avengers.remove(0);
         System.out.println(avengers);
 
+        // removing in Integer ArrayList
 
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(300);
+        nums.add(800);
+        nums.add(400);
+        nums.add(300);
+        System.out.println(nums);
 
+       // nums.remove(300);  the 300 is an int here, so it is trying to remove index 300
+        nums.remove((Integer) 300); // 300 is being cast to Integer, so it will remove the element
+        System.out.println(nums);
 
     }
 }
