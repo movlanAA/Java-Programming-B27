793246f8a4b6ecd7ed00f9f4de285dbd93d253b4
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-08-05 09:53:26 -0500

    day 31 ArrayList

--- src/day32_arraylist/PracticeFlow.java
+++ src/day32_arraylist/PracticeFlow.java
@@ -83,5 +83,19 @@ Print the ArrayList after each action to see how the change is made
         nums.remove((Integer) 300); // 300 is being cast to Integer, so it will remove the element
         System.out.println(nums);
 
+        // Name in ArrayList
+        ArrayList<Character> name = new ArrayList<>();
+        name.add('S');
+        name.add('a');
+        name.add('i');
+        name.add('m');
+
+        System.out.println(name.get(0));
+        System.out.println(name.get(1));
+        System.out.println(name.get(2));
+        System.out.println(name.get(3));
+
+
+
     }
 }
