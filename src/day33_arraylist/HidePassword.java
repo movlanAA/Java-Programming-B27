b71afb659c30d08fc30dcb9e093298fa10f253df
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-08-08 09:52:48 -0500

    day 33 ArrayList

---
+++ src/day33_arraylist/HidePassword.java
@@ -0,0 +1,66 @@
+package day33_arraylist;
+
+import java.util.ArrayList;
+
+public class HidePassword {
+    /*
+    Hide passwords
+
+    Given an ArrayList of passwords (String). Hide each password in a star (*) format where each character is a star and print the ArrayList of hidden passwords
+    Ex:
+    Input:
+    {"one", "hi", "hold}
+
+    Output:
+    [ ***, **, **** ]
+
+     */
+
+    public static ArrayList<String> hidePassword(ArrayList<String> list){
+
+        ArrayList<String> hidden = new ArrayList<>();
+
+        for(String each : list){
+            hidden.add(numOfStars(each.length()));
+        }
+
+        return hidden;
+    }
+
+    public static String numOfStars(int length){
+        String stars = "";
+
+        for(int i = 0; i < length; i++){
+            stars += "*";
+        }
+        return stars;
+    }
+
+
+}
+
+/*
+
+
+   original version
+
+    public static ArrayList<String> hidePassword(ArrayList<String> list){
+
+        ArrayList<String> hidden = new ArrayList<>();
+
+        for(String each : list){
+
+            String stars = "";
+
+            for(int i = 0; i < each.length(); i++){
+                stars += "*";
+            }
+
+            hidden.add(stars);
+
+        }
+
+        return hidden;
+    }
+
+ */
