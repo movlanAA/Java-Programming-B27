314dea7dc3d0289791bbadfcaff6ed33a50af858
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-08-22 12:05:09 -0500

    day 39 inheritance

--- src/day39_inheritance/access/b/RunnerB.java
+++ src/day39_inheritance/access/b/RunnerB.java
@@ -1,6 +1,6 @@
 package day39_inheritance.access.b;
 
-public class RunnerB {
+public class RunnerB{
     public static void main(String[] args) {
 
         // Q: What does TownHouse inherit from House?
@@ -9,7 +9,7 @@ public class RunnerB {
 
         TownHouse house = new TownHouse();
         house.address = "123134 road";
-//        house.city = "Fairfax"; no accessible outside the package
+//        house.city = "Fairfax"; //no accessible outside the package
 //        house.state;  state is default, so only accessible in the same package
 //        house.zipcode zipcode is private, so only accessible in the same class
 
