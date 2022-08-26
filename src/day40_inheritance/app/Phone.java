b2a11896b957736e6cc8fac0dda1f5c34524c62b
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-08-26 09:59:46 -0500

    day 40 inheritance

---
+++ src/day40_inheritance/app/Phone.java
@@ -0,0 +1,14 @@
+package day40_inheritance.app;
+
+public class Phone {
+    public static void main(String[] args) {
+
+        Youtube youtube = new Youtube(4.8);
+        System.out.println(youtube.name);
+        System.out.println(youtube.version);
+        youtube.download();
+        youtube.watchVideo();
+
+
+    }
+}
