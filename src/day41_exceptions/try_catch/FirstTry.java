2e8ca8ccb9736e6758a28587c0c9dd6622d4a250
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-08-29 12:07:37 -0500

    day 41 exceptions

--- src/day41_exceptions/try_catch/FirstTry.java
+++ src/day41_exceptions/try_catch/FirstTry.java
@@ -16,7 +16,7 @@ public class FirstTry {
         System.out.println("Hello");
 
         try{
-            Thread.sleep(-5000);
+            Thread.sleep(5000);
         }catch (Exception e){
             System.out.println("Invalid time given");
         }
