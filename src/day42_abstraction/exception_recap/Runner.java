40a2aa0006fbc0a76afb24e952cd8b8e15f5e49e
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-09-02 11:02:44 -0500

    day 42 recap

--- src/day42_abstraction/exception_recap/Runner.java
+++ src/day42_abstraction/exception_recap/Runner.java
@@ -10,6 +10,9 @@ public class Runner {
             System.out.println(VendingMachine.selectValid(5));
         } catch (InvalidSelectionException e){
             System.out.println(e.getMessage());
+        } finally {
+            // block of code that will run always
+            // you could stop this block from running if you use System.exit() before this block
         }
 
     }
@@ -19,4 +22,7 @@ public class Runner {
 //    }
 
     // using throws will allow the program to compile, but you didn't handle the exception, so when we give the invalid data the exception happens and programs stops
+
+    // if you want to ignore multiple use a comma
+    // throws Exception1, Exception2...
 }
