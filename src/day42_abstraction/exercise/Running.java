e587be9dce752fbce32045b77caca3b7e1dfbd3f
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-09-02 14:06:01 -0500

    day 42 abstraction

---
+++ src/day42_abstraction/exercise/Running.java
@@ -0,0 +1,25 @@
+package day42_abstraction.exercise;
+
+public class Running extends Exercise{
+
+    @Override
+    public void perform(){
+        System.out.println("We are running");
+    }
+
+    @Override
+    public void getCaloriesBurned(int minutes){
+        System.out.println(minutes * 15);
+    }
+
+}
+
+/*
+
+    Running is an Exercise
+    Running is non-abstract (normal class)
+    Running is the first non-abstract class it is the concrete class
+
+    Concrete classes must implement(override) all the abstract methods
+
+ */
