52520337a14b7bca24c742195c58575679210ed3
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-09-13 10:02:27 -0500

    day 44 polymorphism

---
+++ src/day44_polymorphism/employee/Tester.java
@@ -0,0 +1,17 @@
+package day44_polymorphism.employee;
+
+public final class Tester extends Employee{
+
+    public Tester(String name, String jobTitle, double salary) {
+        super(name, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println("running test cases");
+    }
+
+    public void writeBugReport(){
+        System.out.println("Found bug, writing report");
+    }
+}
