7ca296910159e0fdcf4fdb097e16c4ca47d20977
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-09-19 09:39:20 -0500

    day45_polymorphism

---
+++ src/day45_polymorphism/building/HasBalcony.java
@@ -0,0 +1,11 @@
+package day45_polymorphism.building;
+
+public interface HasBalcony {
+    /*
+    Create an interface HasBalcony
+    abstract method:
+        void openBalcony()
+     */
+
+    void openBalcony();
+}
---
+++ src/day45_polymorphism/building/OfficeBuilding.java
@@ -0,0 +1,39 @@
+package day45_polymorphism.building;
+
+public class OfficeBuilding extends Building implements HasElevator{
+    /*
+Create an class OfficeBuilding
+    child class of Building
+    implements HasElevator
+
+    create instance variable
+        int number of floors
+
+    create a constructor to assign the location, price, and number of floors
+
+    implement all abstract methods (give some print statements)
+
+    override the toString to print the location, price, and number of floors
+     */
+
+    int numberOfFloors;
+
+    public OfficeBuilding(String location, double price, int numberOfFloors) {
+        super(location, price);
+        this.numberOfFloors = numberOfFloors;
+    }
+
+    @Override
+    public void pay() {
+        System.out.println("Paying rent for office space");
+    }
+
+    @Override
+    public void elevator(int floor) {
+        System.out.println("we are going to floor " + floor);
+    }
+
+    public String toString(){
+        return super.toString() + " Number of floors: " + numberOfFloors;
+    }
+}
